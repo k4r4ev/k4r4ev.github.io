ea092257170fc6c2da784f2c812fc07cdfcfd0ae
karaev andrey <karaevandrey2000@gmail.com>
2019-11-18 21:40:03 +0300

    Bug fix 	modified:   todo-list/index.html 	modified:   todo-list/script.js 	modified:   todo-list/style.css

--- todo-list/script.js
+++ todo-list/script.js
@@ -1,5 +1,5 @@
-let d = 2;
-let t = 4;
+let d = 1;
+let t = 1;
 
 function createDesk() {
     let desk = document.createElement("div");
@@ -28,76 +28,50 @@ function createDesk() {
     //LIST
     let ul = document.createElement("ul");
     ul.className = "list";
-
-    let li1 = document.createElement("li");
-    li1.innerHTML = "task #1";
-    li1.setAttribute("contenteditable", "true");
-    li1.id = "t" + t;
-    ul.appendChild(li1);
-    let span1 = document.createElement("span");
-    span1.id = "span" + t;
-    span1.innerHTML = "<a onclick=deleteTask(" + t + ")>delete</a>&nbsp;<a onclick='changeTask();'>change</a>";
-    t++;
-    ul.appendChild(span1);
-
-    let li2 = document.createElement("li");
-    li2.innerHTML = "task #2";
-    li2.setAttribute("contenteditable", "true");
-    li2.id = "t" + t;
-    ul.appendChild(li2);
-    let span2 = document.createElement("span");
-    span2.id = "span" + t;
-    span2.innerHTML = "<a onclick='deleteTask(" + t + ");'>delete</a>&nbsp;<a onclick='changeTask();'>change</a>";
-    t++;
-    ul.appendChild(span2);
-
-    let li3 = document.createElement("li");
-    li3.innerHTML = "task #3";
-    li3.setAttribute("contenteditable", "true");
-    li3.id = "t" + t;
-    ul.appendChild(li3);
-    let span3 = document.createElement("span");
-    span3.id = "span" + t;
-    span3.innerHTML = "<a onclick='deleteTask(" + t + ");'>delete</a>&nbsp;<a onclick='changeTask();'>change</a>";
-    t++;
-    ul.appendChild(span3);
+    ul.id = "ul" + d;
+    for (let i = 0; i < 3; i++) {
+        createTask(ul);
+    }
     desk.appendChild(ul);
     desk.appendChild(hr);
     //FOOTER
     let footer = document.createElement("div");
-    let input1 = document.createElement("input");
-    input1.setAttribute("type", "text");
-    input1.setAttribute("placeholder", "Name of new task");
-    footer.appendChild(input1);
-    let input2 = document.createElement("input");
-    input2.setAttribute("type", "button");
-    input2.setAttribute("onclick", "createTask();");
-    input2.setAttribute("value", "Add new task");
-    footer.appendChild(input2);
+    let input = document.createElement("input");
+    input.setAttribute("type", "button");
+    input.setAttribute("onclick", "createTask(ul" + d + ");");
+    input.setAttribute("value", "Add new task");
+    footer.appendChild(input);
     desk.appendChild(footer);
     document.getElementById("container").appendChild(desk);
-    alert("Desk has been created");
     d++;
 }
 
 function deleteDesk(id) {
     document.getElementById(id.remove());
-    alert("Desk has been deleted");
 }
 
-function createTask() {
-    alert("Task has been created");
+function createTask(ul) {
+    let li = document.createElement("li");
+    li.innerHTML = "task #" + t;
+    li.setAttribute("contenteditable", "true");
+    li.id = "t" + t;
+    let span = document.createElement("span");
+    span.id = "span" + t;
+    let a = document.createElement("a");
+    a.setAttribute("onclick", "deleteTask(" + t + ")");
+    a.innerHTML = "delete";
+    span.appendChild(a);
+    ul.appendChild(li);
+    ul.appendChild(span);
+    t++;
 }
 
 function deleteTask(id) {
-    alert(id);
     let task = "t" + id;
-    console.log('task',task);
-    console.log('taskdelete',document.getElementById(task).remove());
-    document.getElementById(task.remove());
+    console.log('task', task);
+    document.getElementById(task).remove();
     let span = "span" + id;
-    document.getElementById(span.remove());
-    alert("Task has been deleted");
+    document.getElementById(span).remove();
 }
 
 function changeTask() {
