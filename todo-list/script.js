b1ca1eaa147d7cffecdd22e8da8cbb9f30a10ba1
karaev andrey <karaevandrey2000@gmail.com>
2019-11-21 15:14:43 +0300

    Bug fixes 	modified:   index.html 	modified:   todo-list/script.js 	modified:   todo-list/style.css

--- todo-list/script.js
+++ todo-list/script.js
@@ -11,11 +11,10 @@ function createDesk() {
     let titleText = document.createElement("h2");
     titleText.setAttribute("contenteditable", "true")
     let titleCaption = document.getElementById('deskName').value;
-    if (titleCaption != "") {
+    if (titleCaption != "")
         titleText.innerHTML = titleCaption;
-    } else {
+    else
         titleText.innerHTML = "The name of the desk";
-    }
     document.getElementById('deskName').value = null;
     let titleButton = document.createElement("a");
     let deleteDesk = "deleteDesk(d" + d + ");";
@@ -26,19 +25,18 @@ function createDesk() {
     desk.appendChild(title);
     desk.appendChild(document.createElement("hr"));
     //LIST
-    let ul = document.createElement("ul");
-    ul.className = "list";
-    ul.id = "ul" + d;
-    for (let i = 0; i < 3; i++) {
-        createTask(ul);
-    }
-    desk.appendChild(ul);
+    let ol = document.createElement("ol");
+    ol.className = "list";
+    ol.id = "ol" + d;
+    for (let i = 0; i < 1; i++)
+        createTask(ol);
+    desk.appendChild(ol);
     desk.appendChild(document.createElement("hr"));
     //FOOTER
     let footer = document.createElement("div");
     let input = document.createElement("input");
     input.setAttribute("type", "button");
-    input.setAttribute("onclick", "createTask(ul" + d + ");");
+    input.setAttribute("onclick", "createTask(ol" + d + ");");
     input.setAttribute("value", "Add new task");
     footer.appendChild(input);
     desk.appendChild(footer);
@@ -50,19 +48,23 @@ function deleteDesk(id) {
     document.getElementById(id.remove());
 }
 
-function createTask(ul) {
+function createTask(ol) {
     let li = document.createElement("li");
     li.innerHTML = "task #" + t;
     li.setAttribute("contenteditable", "true");
     li.id = "t" + t;
     let span = document.createElement("span");
     span.id = "span" + t;
-    let a = document.createElement("a");
-    a.setAttribute("onclick", "deleteTask(" + t + ")");
-    a.innerHTML = "delete";
-    span.appendChild(a);
-    ul.appendChild(li);
-    ul.appendChild(span);
+    let del = document.createElement("a");
+    del.setAttribute("onclick", "deleteTask(" + t + ")");
+    del.innerHTML = "delete";
+    let complete = document.createElement("a");
+    complete.setAttribute("onclick", "completeTask(" + t + ")");
+    complete.innerHTML = "complete";
+    span.appendChild(del);
+    span.appendChild(complete);
+    ol.appendChild(li);
+    ol.appendChild(span);
     t++;
 }
 
@@ -71,6 +73,8 @@ function deleteTask(id) {
     document.getElementById("span" + id).remove();
 }
 
-function changeTask() {
-    alert("Task has been changed");
+function completeTask(id) {
+    document.getElementById("t" + id).classList.add('complete');
+    document.getElementById("t" + id).setAttribute("contenteditable", "false");
+    document.getElementById("span" + id).remove();
 }
