2deabb86b28ac811b2f4fad62083588b9dcf790a
karaev andrey <karaevandrey2000@gmail.com>
2019-12-12 14:32:30 +0300

    Bug fixes

--- snake-js/scripts/script.js
+++ snake-js/scripts/script.js
@@ -53,13 +53,13 @@ document.addEventListener("keydown", direction);
 let dir;
 
 function direction(event) {
-  if (event.keyCode == 37 && dir != "right")
+  if ((event.keyCode == 37 && dir != "right") || (event.keyCode == 65 && dir != "right"))
     dir = "left";
-  else if (event.keyCode == 38 && dir != "down")
+  else if ((event.keyCode == 38 && dir != "down") || (event.keyCode == 87 && dir != "down"))
     dir = "up";
-  else if (event.keyCode == 39 && dir != "left")
+  else if ((event.keyCode == 39 && dir != "left") || (event.keyCode == 68 && dir != "left"))
     dir = "right";
-  else if (event.keyCode == 40 && dir != "up")
+  else if ((event.keyCode == 40 && dir != "up") || (event.keyCode == 83 && dir != "up"))
     dir = "down";
 }
 
